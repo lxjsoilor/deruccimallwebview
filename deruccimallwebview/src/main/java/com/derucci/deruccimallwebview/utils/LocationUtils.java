64c2e182d6a8e17f8ed5980a6eb73fab8b24184c
sailor <416596401@qq.com>
2023-06-21 14:45:25 +0800

    Update LocationUtils.java

--- deruccimallwebview/src/main/java/com/derucci/deruccimallwebview/utils/LocationUtils.java
+++ deruccimallwebview/src/main/java/com/derucci/deruccimallwebview/utils/LocationUtils.java
@@ -4,14 +4,11 @@ import android.Manifest;
 import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Context;
-import android.location.Criteria;
 import android.location.Location;
 import android.location.LocationManager;
-
 import com.google.android.gms.maps.model.LatLng;
-
+import java.util.List;
 import pub.devrel.easypermissions.EasyPermissions;
-
 /**
  * Author: 林雄军
  * Description: Description
@@ -26,17 +23,29 @@ public class LocationUtils {
         if (!EasyPermissions.hasPermissions(context, Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION)) {
             String[] perms = {Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION};
             EasyPermissions.requestPermissions((Activity) context, "获取定位", REQUEST_LOCATION_CODE, perms);
+            return null;
         }
-        LocationManager locationManager = (LocationManager) context.getSystemService(Context.LOCATION_SERVICE);
-        Criteria criteria = new Criteria();
-        String provider = locationManager.getBestProvider(criteria, false);
-        Location location = null;
-        if (provider != null) {
-            location = locationManager.getLastKnownLocation(provider);
-        }
+        Location location = getLastKnownLocation(context);
         if (location != null) {
             return new LatLng(location.getLatitude(), location.getLongitude());
         }
         return null;
+    }
+
+    @SuppressLint("MissingPermission")
+    private static Location getLastKnownLocation(Context context) {
+        LocationManager locationManager = (LocationManager) context.getSystemService(Context.LOCATION_SERVICE);
+        List<String> providers = locationManager.getProviders(true);
+        Location bestLocation = null;
+        for (String provider : providers) {
+            Location l = locationManager.getLastKnownLocation(provider);
+            if (l == null) {
+                continue;
+            }
+            if (bestLocation == null || l.getAccuracy() < bestLocation.getAccuracy()) {
+                bestLocation = l;
+            }
+        }
+        return bestLocation;
     }
 }
