d72d22778e60ff89f81cf94412024a323e498125
sailor <416596401@qq.com>
2023-07-03 09:08:06 +0800

    Update KeyConfig.java

--- deruccimallwebview/src/main/java/com/derucci/deruccimallwebview/config/KeyConfig.java
+++ deruccimallwebview/src/main/java/com/derucci/deruccimallwebview/config/KeyConfig.java
@@ -16,7 +16,7 @@ public class KeyConfig {
     static public String H5_URL = "http://10.16.2.79:30452";
     static public String H5_URL_DEV = "http://10.16.2.79:30452";
     static public String H5_URL_TEST = "http://10.16.2.79:30452";
-    static public String H5_URL_UAT = "http://10.34.24.100:81";
+    static public String H5_URL_UAT = "http://10.16.2.79:30452";
 
     public enum ENV {
         PROD,
